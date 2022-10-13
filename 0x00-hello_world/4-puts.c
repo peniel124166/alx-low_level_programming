bf5e496e6e29b6729d5f010d9a662c010ce86da2
peniel nunu <penielnunuu@gmail.com>
2022-10-13 11:11:29 -0700

    third commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*
+ * main - printing
+ *
+ * Return: Always 0 (successful)
+ */
+
+int main(void)
+
+{
+
+	    puts( "\"Programming is like building a multilingual puzzle"");
+
+	    return (0);
+
+}
