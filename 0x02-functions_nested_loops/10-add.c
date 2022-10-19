7e642e6e18c91aceceba5b41c06ff98064e762c8
peniel nunu <penielnunuu@gmail.com>
2022-10-19 00:04:22 -0700

    4 commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * int add - adds two integers
+ * @a: the first number
+ * @b: the second number
+ * Return: the result
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -4,15 +4,13 @@
  * @c: the number to print
  * Return: the last digit
  */
-int print_last_digit(int)
+int print_last_digit(int n)
 {
-	int n;
-
 	int ld = n % 10;
 
 	if (ld < 0)
 		ld *= -1;
 
-	-putchar(ld + '0');
+	_putchar(ld + '0');
 	return (0);
 }
