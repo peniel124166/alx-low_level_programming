d7dc9406f97814ab634b5c304c1e13063e098d50
peniel nunu <penielnunuu@gmail.com>
2022-10-31 13:13:34 -0700

    2 commit

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memcpy -  copies memory area.
+ * @dest: copy to.
+ * @src: copy from.
+ * @n: number of bytes copy from @src.
+ *
+ * Return: a pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int index;
+	unsigned char *destination = dest;
+	const unsigned char *source = src;
+
+	for (index = 0; index < n; index++)
+		destination[index] = source[index];
+
+	return (dest);
+}
