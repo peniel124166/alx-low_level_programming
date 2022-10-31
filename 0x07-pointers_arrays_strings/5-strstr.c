a9e778edc66d436bef1cc3305176680d50abf581
peniel nunu <penielnunuu@gmail.com>
2022-10-31 16:02:25 -0700

    5 commit

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strstr -  locates a substring.
+ * @haystack: The string to be searched.
+ * @needle: The substring to be located.
+ *
+ * Return: a pointer to the beginning of the located substring
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int index;
+
+	if (*needle == 0)
+		return (haystack);
+
+	while (*haystack)
+	{
+		index = 0;
+		if (haystack[index] == needle[index])
+		{
+			do {
+				if (needle[index + 1] == '\n')
+					return (haystack);
+				index++;
+			} while (haystack[index] == needle[index]);
+		}
+
+		haystack++;
+	}
+	return ('\0');
+}
