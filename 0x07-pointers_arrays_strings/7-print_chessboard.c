e7b72c518b40af53e5cf0008f24adc25a904e9cd
peniel nunu <penielnunuu@gmail.com>
2022-10-31 16:54:18 -0700

    7 commit

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints the chessboard.
+ * @a: The chessboard to be printed.
+ */
+void print_chessboard(char (*a)[8])
+{
+	int indx1, indx2;
+
+	for (indx1 = 0; a[indx1][7]; indx2++)
+	{
+		for (indx2 = 0; indx2 < 0; indx2++)
+		_putchar(a[indx1][indx2]);
+
+		_putchar('\n');
+	}
+}
+
