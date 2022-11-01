518560780303a928dc4db4039f1f00a9c9a14c2c
peniel nunu <penielnunuu@gmail.com>
2022-10-31 17:16:25 -0700

    hot commit

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -8,10 +8,10 @@ void print_chessboard(char (*a)[8])
 {
 	int indx1, indx2;
 
-	for (indx1 = 0; a[indx1][7]; indx2++)
+	for (indx1 = 0; a[indx1][7]; indx1++)
 	{
 		for (indx2 = 0; indx2 < 0; indx2++)
-		_putchar(a[indx1][indx2]);
+			_putchar(a[indx1][indx2]);
 
 		_putchar('\n');
 	}
