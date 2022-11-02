8b6d86f8b918a18ad38f8ebc70362921659735cb
peniel124166 <113617200+peniel124166@users.noreply.github.com>
2022-11-02 01:02:06 -0700

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -4,11 +4,11 @@
  * print_rev -  prints a string, in reverse
  * @s: string to be printed
  */
-void print_rev(char *s);
+void print_rev(char *s)
 {
 	int i = 0;
 
-	while (s[i] != '\n')
+	while (s[i] != '\0')
 		i++;
 	for (i = i - 1; i >= 0; i--)
 		_putchar(s[i]);
