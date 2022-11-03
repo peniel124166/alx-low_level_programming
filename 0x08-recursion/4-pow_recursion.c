771c9906a6e31ebfed5556738aaf4b6b0f045356
peniel nunu <penielnunuu@gmail.com>
2022-11-03 13:09:17 -0700

    hot fix

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -9,11 +9,9 @@
  */
 int _pow_recursion(int x, int y)
 {
-	int result = x;
-
 	if (y < 0)
 		return (-1);
-
-	else if (y == 0)
+	if (y == 0)
 		return (1);
+	return (x * _pow_recursion(x, y - 1));
 }
