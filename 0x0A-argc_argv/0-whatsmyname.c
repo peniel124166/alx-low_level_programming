2b81cc5e6bd084c6d9e4bc5eaff719d7bbea4003
peniel nunu <penielnunuu@gmail.com>
2022-11-04 12:24:04 -0700

    first commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints its name
+ * @argc: count argument
+ * @argv: Arguments
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
