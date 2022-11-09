b9109d66313e220d6c38216227bc62fe6f9208ab
peniel nunu <penielnunuu@gmail.com>
2022-11-09 03:54:22 -0800

    2 commit

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -30,14 +30,10 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL)
 		s1 = "";
-	
 	if (s2 == NULL)
 		s2 = "";
-	
 	size = (_strlen(s1) + _strlen(s2) + 1);
-	
 	dst  = (char *) malloc(size * sizeof(char));
-	
 	if (dst == 0)
 	{
 	return (NULL);
