38174eba8558f8612a0bc9d2faf7c35a323c3263
peniel124166 <113617200+peniel124166@users.noreply.github.com>
2022-11-09 06:09:21 -0800

    Update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -54,7 +54,7 @@ char *_strdup(char *str)
 		return (NULL);
 	}
 	size = _strlen(str) + 1;
-	dst = (char * ) malloc(size *sizeof(char));
+	dst = (char *) malloc(size *sizeof(char));
 	if (dst == 0)
 	{
 		return (NULL);
