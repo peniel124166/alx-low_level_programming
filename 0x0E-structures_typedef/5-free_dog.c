f197ec2cf4087752ea4037d50befa0da00a613c5
peniel124166 <113617200+peniel124166@users.noreply.github.com>
2022-11-14 03:25:34 -0800

    Update 5-free_dog.c

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,2 +1,16 @@
-5-free_dog.c
+#include <stdlib.h>
+#include "dog.h"
 
+/**
+ * free_dog - frees memory allocated for a struct dog
+ * @d: struct dog to free
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
