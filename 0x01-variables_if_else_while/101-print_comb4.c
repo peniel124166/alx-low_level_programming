a1395c37a4b65d9822220e1669365436dc87d432
peniel124166 <113617200+peniel124166@users.noreply.github.com>
2022-11-28 08:58:39 -0800

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,24 +8,23 @@
 int main(void)
 {
 	int num1, num2, num3;
-	
 	for (num1 = 0; num1 <= 9; num1++)
-	                {
-			for (num2 = num1 + 1; num2 <= 9; num2++)
-			{
+	{
+		for (num2 = num1 + 1; num2 <= 9; num2++)
+		{
 			for (num3 = num2 + 1; num3 <= 9; num3++)
 			{
-			putchar (num1 + '0');
-			putchar (num2 + '0');
-			putchar (num3 + '0');
-			if (num1 < 7)
-			{
-			putchar(',');
-			putchar(' ');
+				putchar (num1 + '0');
+				putchar (num2 + '0');
+				putchar (num3 + '0');
+				if (num1 < 7)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
-			}
-			}
-			}
-			putchar('\n');
-			return (0);
+		}
+	}
+	putchar('\n');
+	return (0);
 }
