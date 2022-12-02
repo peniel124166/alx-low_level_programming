e4742045259cc28d1277b799854ec437797b4df7
peniel124166 <113617200+peniel124166@users.noreply.github.com>
2022-12-02 06:25:30 -0800

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,7 +3,7 @@
 /**
  * _isalpha - Write a function that checks for alphabetic character
  * @c: the character to be checked
- * Return: 1 if c is a letter, 0 otherwise
+ * Return: 1 if c is a letter(lowercase or uppercase), 0 otherwise
  */
 int _isalpha(int c)
 {
