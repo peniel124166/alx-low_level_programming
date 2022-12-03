3683d93bba00da74252acebe0d068d070d5220fd
peniel124166 <113617200+peniel124166@users.noreply.github.com>
2022-12-03 02:38:40 -0800

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,24 +6,26 @@
  */
 void jack_bauer(void)
 {
-    int a, b, c, d;
-    
-    for (a = 0; a <= 2; a++)
-    {
-        for (b = 0; b <= 3; b++)
-        {
-            for (c = 0; c <= 5; c++)
-            {
-                for (d = 0; d <= 9; d++)
-                {
-                    _putchar(a + '0');
-                    _putchar(b + '0');
-                    _putchar(':');
-                    _putchar(c + '0');
-                    _putchar(d + '0');
-                    _putchar('\n');
-                }
-            }
-        }
-    }
+	int a, b, c, d;
+
+	for (a = 48; a <= 50; a++)
+	{
+		for (b = 48; b <= 57; b++)
+		{
+			for (c = 48; c <= 53; c++)
+			{
+				for (d = 48; d <= 57; d++)
+				{
+					if (a >= 50 && b >= 52)
+					break;
+					_putchar(a);
+					_putchar(b);
+					_putchar(58);
+					_putchar(c);
+					_putchar(d);
+					_putchar('\n');
+				}
+			}
+		}
+	}
 }
