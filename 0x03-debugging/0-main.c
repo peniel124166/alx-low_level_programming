6e50f41e5766d25ee384674fadd56623de7b4018
peniel124166 <113617200+peniel124166@users.noreply.github.com>
2022-12-09 02:34:29 -0800

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,1 +1,16 @@
-waa
+#include "main.h"
+
+/**
+ * main - tests funtion that prints if integer is positive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
