e5afff2e758d9a62db1d7a4f6f4cedcf211c65bc
peniel124166 <113617200+peniel124166@users.noreply.github.com>
2023-01-16 13:38:14 -0800

    Update 6-abs.c

--- 0x18-dynamic_libraries/6-abs.c
+++ 0x18-dynamic_libraries/6-abs.c
@@ -1,1 +1,11 @@
-aaa
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer
+ * @ab: input letrer
+ * Return: Always success
+ */
+int _abs(int ab)
+{
+
+return (ab * ((ab > 0) - (ab < 0)));
+}
