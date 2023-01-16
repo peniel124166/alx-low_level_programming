70393536750380fb19ec49eff680fc7c08feb4db
peniel124166 <113617200+peniel124166@users.noreply.github.com>
2023-01-16 13:23:29 -0800

    Update 0-putchar.c

--- 0x18-dynamic_libraries/0-putchar.c
+++ 0x18-dynamic_libraries/0-putchar.c
@@ -1,1 +1,15 @@
-aa
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always success
+ */
+int main(void)
+{
+
+printf("_putchar\n");
+
+return (0);
+}
